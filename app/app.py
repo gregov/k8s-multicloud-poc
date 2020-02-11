b48f4f4206c8b2af50c1e460a6d21b8e03cd33da
Greg Reboul <gregoire.reboul@gmail.com>
2020-02-11 11:34:57 -0500

    Creating the docker app

---
+++ app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from pprint import pprint
+import os
+import json
+
+
+app = Flask(__name__)
+
+statefile = "data/data.json"
+
+@app.route('/')
+def home():
+    pprint(os.environ)
+    remote_ip = request.remote_addr
+    if os.path.exists(statefile):
+        with open(statefile, "r") as f:
+            history = json.load(f)
+    else:
+        history = []
+
+    result = jsonify({'my_hostname': request.hostname,
+                      'your_ip': remote_ip,
+                      'history': history
+                     }), 200
+
+    if remote_ip not in history:
+        history.append(remote_ip)
+
+    with open(statefile, "w+") as f:
+        history = json.dump(history, f)    
+
+
+    return result
